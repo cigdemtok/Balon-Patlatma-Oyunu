300e8818cda6e06e90f0a095665102dcd0c347b7
cigdemtok <50103647+cigdemtok@users.noreply.github.com>
2021-07-12 23:34:55 +0300

    Unity ile geliştirilen mobil çocuk oyunu

---
+++ Assets/BalonKontrolcu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalonKontrolcu : MonoBehaviour
+{
+    public GameObject patlama;
+    OyunKontrolcu oyunkontrolscripti; //nesne oluşturma
+
+    void Start()
+    {
+        oyunkontrolscripti=GameObject.Find("GameObject").GetComponent<OyunKontrolcu>();
+    }
+
+    void OnMouseDown()
+    {
+        GameObject go= Instantiate(patlama, transform.position,transform.rotation) as GameObject;
+        Destroy(this.gameObject);
+        Destroy(go,0.3f);
+        oyunkontrolscripti.BalonEkle();
+    }
+}
---
+++ Assets/BalonOlusturucu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalonOlusturucu : MonoBehaviour
+{
+    public GameObject balon;
+    float balonOlusturmaSuresi= 1f;
+    public float zamanSayaci =0f;
+    OyunKontrolcu okScripti;
+   
+    void Start()
+    {
+        okScripti=this.gameObject.GetComponent<OyunKontrolcu>();
+    }
+
+    void Update()
+    {
+        zamanSayaci-=Time.deltaTime;
+        int katsayi=(int)(okScripti.zamanSayaci/10)-6;
+        katsayi*=-1;
+
+        if(zamanSayaci<0 && okScripti.zamanSayaci>0) 
+        {
+            GameObject go= Instantiate(balon ,new Vector3(Random.Range(-8.0f,8.0f),-6f,0), Quaternion.Euler(0,0,0)) as GameObject;
+            go.GetComponent<Rigidbody2D>().AddForce(new Vector3(0,Random.Range(30f*katsayi,80f*katsayi),0));
+            zamanSayaci=balonOlusturmaSuresi;
+        }
+    }
+}
